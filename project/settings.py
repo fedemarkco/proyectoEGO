609ca6935e5deb9675744209e77cb9d9533e1c4c
Marco <fedemarkco@gmail.com>
2023-06-09 01:26:29 -0300

    Change media

--- project/settings.py
+++ project/settings.py
@@ -121,6 +121,8 @@ USE_THOUSAND_SEPARATOR = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
